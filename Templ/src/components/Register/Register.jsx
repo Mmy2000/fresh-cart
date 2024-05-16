1dd464b6a824cf26c83c63f7393549aa0ce35cad
Mmy2000 <yahianada005@gmail.com>
2024-05-16 20:56:22 +0300

    creating validation using yup for register component

--- Templ/src/components/Register/Register.jsx
+++ Templ/src/components/Register/Register.jsx
@@ -40,7 +40,8 @@ export default function Register() {
     name : Yup.string().min(3,'too short').max(10 , 'too long').required('Name is required'),
     email : Yup.string().email('Email is invalid').required('Email is required'),
     phone : Yup.string().matches('/^01[0125][0-9]{8}$/' , 'Phone is invalid').required('Phone is required'),
-    password : Yup.string().matches('/^$/' , 'Phone is invalid').required('Password is required'),
+    password : Yup.string().matches(/^[A-Z][a-z0-9]{5,10}$/ , 'Password must start with uppercase ..').required('Password is required'),
+    rePassword : Yup.string().oneOf([Yup.ref('password')] , "dont't match").required('Repassword is required'),
   })
 
   let formik = useFormik({
@@ -52,6 +53,7 @@ export default function Register() {
       rePassword:''
     },
     // validate:myValidation,
+    validationSchema:yupValidation,
     onSubmit:handleRegister
   })
 
@@ -82,14 +84,23 @@ export default function Register() {
       <input type="phone" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.phone} name="phone" id="phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" "  />
       <label htmlFor="phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter Your Phone number :</label>
   </div>
+  {formik.errors.phone && formik.touched.phone?<div class="p-4 mb-2 text-sm text-red-800 rounded-lg bg-red-50 font-medium dark:bg-gray-800 dark:text-red-400" role="alert">
+  {formik.errors.phone}
+</div>:null}
   <div className="relative z-0 w-full mb-5 group">
       <input type="password" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.password} name="password" id="password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" "  />
       <label htmlFor="password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter Your Password :</label>
   </div>
+  {formik.errors.password && formik.touched.password?<div class="p-4 mb-2 text-sm text-red-800 rounded-lg bg-red-50 font-medium dark:bg-gray-800 dark:text-red-400" role="alert">
+  {formik.errors.password}
+</div>:null}
   <div className="relative z-0 w-full mb-5 group">
       <input type="password" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.rePassword} name="rePassword" id="rePassword" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" "  />
       <label htmlFor="rePassword" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Repeat Your Password :</label>
   </div>
+  {formik.errors.rePassword && formik.touched.rePassword?<div class="p-4 mb-2 text-sm text-red-800 rounded-lg bg-red-50 font-medium dark:bg-gray-800 dark:text-red-400" role="alert">
+  {formik.errors.rePassword}
+</div>:null}
     <button type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
 
   </form>
