ab1bf603aaea1108d7162a349093f037e0b69de3
Mmy2000 <yahianada005@gmail.com>
2024-05-16 17:59:03 +0300

    starting with register component

--- Templ/src/components/Register/Register.jsx
+++ Templ/src/components/Register/Register.jsx
@@ -9,8 +9,8 @@ import { Navigate, useNavigate } from 'react-router-dom';
 
 export default function Register() {
 
-  function handleRegister() {
-    console.log("hello");
+  function handleRegister(formValues) {
+    console.log(formValues);
   }
 
   let formik = useFormik({
@@ -28,7 +28,7 @@ export default function Register() {
   return <>
   <div className="py-6 max-w-xl mx-auto">
     <h2 className='text-3xl mb-6 font-bold text-center text-green-600'>Regitser now</h2>
-    <form >
+    <form onSubmit={formik.handleSubmit} >
   <div className="relative z-0 w-full mb-5 group">
       <input type="text" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.name} name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" "  />
       <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter Your Name :</label>
