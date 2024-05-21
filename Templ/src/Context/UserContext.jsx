bfff384a46d1e07ec41071ca91b532a3d1389195
Mmy2000 <yahianada005@gmail.com>
2024-05-21 18:43:33 +0300

    creating context and fixing reload issue

--- Templ/src/Context/UserContext.jsx
+++ Templ/src/Context/UserContext.jsx
@@ -1,10 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export let UserContext = createContext()
 
 export default function UserContextProvider(props) {
 
     const [userLogin, setUserLogin] = useState(null);
+    useEffect(() => {
+        
+        if (localStorage.getItem('userTaken') !== null) {
+            setUserLogin(localStorage.getItem('userTaken'))
+        }
+        
+    }, []);
 
     return <UserContext.Provider value={{userLogin , setUserLogin}}>
         {props.children}
