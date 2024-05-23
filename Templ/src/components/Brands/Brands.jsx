bd88f44ad8159f27fe00bce9f495f6893a6ce8ff
Mmy2000 <yahianada005@gmail.com>
2024-05-23 17:35:16 +0300

    displaying all brands

--- Templ/src/components/Brands/Brands.jsx
+++ Templ/src/components/Brands/Brands.jsx
@@ -1,14 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import Style from './Brands.module.css';
+import axios from 'axios';
 
 
 export default function Brands() {
-    const [counter, setCounter] = useState(0);
-    useEffect(()=>{
 
+
+    const [brands, setBrands] = useState([]);
+
+    function getBrands() {
+      axios.get(`https://ecommerce.routemisr.com/api/v1/brands`)
+      .then( ({data})=>{
+        console.log(data.data);
+        setBrands(data.data)
+      })
+      .catch( ()=>{
+
+      })
+    }
+    useEffect(()=>{
+      getBrands()
     } , []);
   return <>
-    <h1>Brands</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi eligendi accusantium libero quae sit ex reiciendis eaque voluptatem. Sed architecto, voluptatibus ab nobis praesentium laudantium consequatur assumenda error odio maiores!</p>
+    <div className="row">
+      {brands.map( (brand)=> 
+      <div key={brand.id} className='w-1/6 py-4 '>
+        <div className="brand relative py-4 px-4">
+          <img className='w-full' src={brand.image} alt="" />
+          <div className="cover ">
+            <h3 className='text-2xl font-extrabold pt-2'>{brand.name}</h3>
+          </div>
+        </div>
+      </div>)}
+      
+    </div>
   </>
 }
