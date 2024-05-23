ccfa1b9d70352c7171137dc692b44444e435af08
Mmy2000 <yahianada005@gmail.com>
2024-05-23 17:48:28 +0300

    displaying all categories

--- Templ/src/components/Categories/Categories.jsx
+++ Templ/src/components/Categories/Categories.jsx
@@ -1,14 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import Style from './Categories.module.css';
+import axios from 'axios';
+
 
 
 export default function Categories() {
-    const [counter, setCounter] = useState(0);
-    useEffect(()=>{
+    const [categories, setCategories] = useState([]);
 
+    function getCategories() {
+      axios.get(`https://ecommerce.routemisr.com/api/v1/categories`)
+      .then( (({data})=>{
+        setCategories(data.data)
+      }))
+      .catch( ()=>{
+
+      })
+    }
+    useEffect(()=>{
+      getCategories()
     } , []);
   return <>
-    <h1>Categories</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi eligendi accusantium libero quae sit ex reiciendis eaque voluptatem. Sed architecto, voluptatibus ab nobis praesentium laudantium consequatur assumenda error odio maiores!</p>
+  <div className="row">
+    {categories.map( (category)=> 
+      <div key={category.id} className='w-1/6 py-4 '>
+        <div className="brand relative mx-4 ">
+          <img className='w-full h-[300px]' src={category.image} alt="" />
+          <div className="cover  ">
+            <h3 className='text-2xl font-extrabold pt-2'>{category.name}</h3>
+          </div>
+        </div>
+      </div>
+    )}
+  </div>
   </>
 }
