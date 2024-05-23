cc230c06b9c947c87d8b1b848d3fec97b1c569b4
Mmy2000 <yahianada005@gmail.com>
2024-05-23 13:37:33 +0300

    creating slider for categories images

--- Templ/src/components/CategoriesSlider/CategoriesSlider.jsx
+++ Templ/src/components/CategoriesSlider/CategoriesSlider.jsx
@@ -1,15 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import Style from './CategoriesSlider.module.css';
 import Slider from "react-slick";
+import axios from 'axios';
+
 
 
 export default function CategoriesSlider() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 8,
+    slidesToScroll: 3,
+    autoplay:true
+  };
+  const [categories, setCategories] = useState([]);
+
+  function getCategories() {
+    axios.get(`https://ecommerce.routemisr.com/api/v1/categories`)
+    .then( ({data})=>{
+      setCategories(data.data)
+    })
+    .catch( (error)=>{
+
+    })
+  }
     const [counter, setCounter] = useState(0);
     useEffect(()=>{
-
+      getCategories()
     } , []);
   return <>
-    <h1>CategoriesSlider</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi eligendi accusantium libero quae sit ex reiciendis eaque voluptatem. Sed architecto, voluptatibus ab nobis praesentium laudantium consequatur assumenda error odio maiores!</p>
+  <Slider {...settings}>
+    {categories.map( (category)=> <div>
+      <img className='category-image w-full'  src={category.image} alt="" />
+      <h3>{category.name}</h3>
+    </div>)}
+    </Slider>
   </>
 }
