330baa41ee9b64525809f45ad24025bf9ac283d3
Mmy2000 <yahianada005@gmail.com>
2024-05-25 20:31:37 +0300

    creating product by brand component

--- Templ/src/components/BrandDetails/BrandDetails.jsx
+++ Templ/src/components/BrandDetails/BrandDetails.jsx
@@ -3,6 +3,7 @@ import Style from './BrandDetails.module.css';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import { RingLoader } from 'react-spinners';
 
 
 export default function BrandDetails() {
@@ -31,33 +32,41 @@ export default function BrandDetails() {
    }, [ brand]);
   return (
     <>
-      <div className="row">
-        {relatedProducts?.map((product) => (
-          <div key={product.id} className="w-1/6 py-4">
-            <div className="product py-4 px-4">
-              <Link
-                to={`/productdetails/${product.id}/${product.category.name}`}
-              >
-                <img className="w-full" src={product.imageCover} />
-                <span className="block font-light text-green-600">
-                  {product.category.name}
-                </span>
-                <h3 className="text-lg font-normal text-gray-900 mb-4">
-                  {product.title.split(" ").slice(0, 2).join(" ")}
-                </h3>
-                <div className="flex items-center justify-between">
-                  <span>{product.price} EGP</span>
-                  <span>
-                    {product.ratingsAverage}{" "}
-                    <i className="fas fa-star text-yellow-500"></i>
+      {relatedProducts ? (
+        <div className="row">
+          {relatedProducts?.map((product) => (
+            <div key={product.id} className="w-1/6 py-4">
+              <div className="product py-4 px-4">
+                <Link
+                  to={`/productdetails/${product.id}/${product.category.name}`}
+                >
+                  <img className="w-full" src={product.imageCover} />
+                  <span className="block font-light text-green-600">
+                    {product.category.name}
                   </span>
-                </div>
-                <button className="btn">add to cart</button>
-              </Link>
+                  <h3 className="text-lg font-normal text-gray-900 mb-4">
+                    {product.title.split(" ").slice(0, 2).join(" ")}
+                  </h3>
+                  <div className="flex items-center justify-between">
+                    <span>{product.price} EGP</span>
+                    <span>
+                      {product.ratingsAverage}{" "}
+                      <i className="fas fa-star text-yellow-500"></i>
+                    </span>
+                  </div>
+                  <button className="btn">add to cart</button>
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center w-full justify-center">
+          <RingLoader color="green" />
+        </div>
+        
+      )}
+      
     </>
   );
 }
