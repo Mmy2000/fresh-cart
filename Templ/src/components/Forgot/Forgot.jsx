e28dc2a1cc390e241e75df3586028d0277a897b3
Mmy2000 <yahianada005@gmail.com>
2024-06-08 17:51:35 +0300

    adding reset password component

--- Templ/src/components/Forgot/Forgot.jsx
+++ Templ/src/components/Forgot/Forgot.jsx
@@ -8,7 +8,9 @@ import { ToastContainer, toast } from "react-toastify";
 
 export default function Forgot() {
   let navigate = useNavigate();
+  const [isLoading, setisLoading] = useState(false);
   function handleForgot(formValues) {
+    setisLoading(true);
     axios
       .post(
         `https://ecommerce.routemisr.com/api/v1/auth/forgotPasswords`,
@@ -17,6 +19,7 @@ export default function Forgot() {
       .then((apiResponse) => {
         console.log(apiResponse);
         if (apiResponse?.data?.statusMsg === "success") {
+          setisLoading(false);
           toast.success("Reset code sent to your email");
           navigate("/verifyCode");
 
@@ -24,6 +27,7 @@ export default function Forgot() {
       })
       .catch((apiResponse) => {
         console.log(apiResponse);
+        setisLoading(false);
       });
     console.log(formValues);
   }
@@ -49,7 +53,10 @@ export default function Forgot() {
       <input type="email" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email} name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" "  />
       <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter Your Email address :</label>
   </div>
-  <button  type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
+  <button  type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+      {isLoading?<i className='fas fa-spinner fa-spin me-2'></i>:'Submit'}
+      
+      </button>
   </form>
     </div>
   </>
--- Templ/src/components/UpdatePassword/UpdatePassword.jsx
+++ Templ/src/components/UpdatePassword/UpdatePassword.jsx
@@ -9,8 +9,10 @@ import { ToastContainer, toast } from "react-toastify";
 
 export default function UpdatePassword() {
   let navigate = useNavigate()
+  const [isLoading, setisLoading] = useState(false);
   let { setUserLogin } = useContext(UserContext);
   function handleUpdate(formValues) {
+    setisLoading(true);
     axios
       .put(
         `https://ecommerce.routemisr.com/api/v1/auth/resetPassword`,
@@ -21,12 +23,14 @@ export default function UpdatePassword() {
         if (apiResponse?.status === 200 ) {
           localStorage.setItem("userTaken", apiResponse.data.token);
           setUserLogin(apiResponse.data.token);
+          setisLoading(false);
           toast.success("Password updated successfully");
-          navigate("/login");
+          navigate("/");
           console.log(apiResponse);
         }
       })
       .catch((apiResponse) => {
+        setisLoading(false);
         console.log(apiResponse);
       });
     console.log(formValues);
@@ -57,7 +61,10 @@ export default function UpdatePassword() {
       <input type="password" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.newPassword} name="newPassword" id="password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" "  />
       <label htmlFor="password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter Your New Password :</label>
   </div>
-  <button  type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
+  <button  type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+      {isLoading?<i className='fas fa-spinner fa-spin me-2'></i>:'Submit'}
+      
+      </button>
   </form>
     </div>
   </>
