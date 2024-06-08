0a4e61e0f9348d71f084225be4a3a6910d7c95ad
Mmy2000 <yahianada005@gmail.com>
2024-06-08 17:56:02 +0300

    adding reset password component

--- Templ/src/components/UpdatePassword/UpdatePassword.jsx
+++ Templ/src/components/UpdatePassword/UpdatePassword.jsx
@@ -35,11 +35,17 @@ export default function UpdatePassword() {
       });
     console.log(formValues);
   }
+  let yupValidation = Yup.object().shape({
+    newPassword: Yup.string()
+      .matches(/^[A-Z][a-z0-9]{5,10}$/, "Password must start with uppercase ..")
+      .required("Password is required"),
+  });
   let formik = useFormik({
     initialValues: {
       email: "",
       newPassword: "",
     },
+    validationSchema: yupValidation,
     onSubmit: handleUpdate,
   });
     const [counter, setCounter] = useState(0);
@@ -61,6 +67,9 @@ export default function UpdatePassword() {
       <input type="password" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.newPassword} name="newPassword" id="password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" "  />
       <label htmlFor="password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter Your New Password :</label>
   </div>
+  {formik.errors.newPassword && formik.touched.newPassword?<div class="p-4 mb-2 text-sm text-red-800 rounded-lg bg-red-50 font-medium dark:bg-gray-800 dark:text-red-400" role="alert">
+  {formik.errors.newPassword}
+</div>:null}
   <button  type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
       {isLoading?<i className='fas fa-spinner fa-spin me-2'></i>:'Submit'}
       
