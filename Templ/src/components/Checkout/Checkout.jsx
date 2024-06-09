7e57125826a749f5d597238f24b58ca6c015158b
Mmy2000 <yahianada005@gmail.com>
2024-06-09 14:04:15 +0300

    adding checkout component

--- Templ/src/components/Checkout/Checkout.jsx
+++ Templ/src/components/Checkout/Checkout.jsx
@@ -6,9 +6,9 @@ export default function Checkout() {
   let formik = useFormik({
     initialValues: {
       shippingAddress: {
-        details: "details",
-        phone: "01010800921",
-        city: "Cairo",
+        details: "",
+        phone: "",
+        city: "",
       },
     },
     onSubmit:(formValues)=>{
@@ -20,54 +20,70 @@ export default function Checkout() {
       <h2 className="text-2xl font-bold mt-4 text-green-600">
         Shipping Address
       </h2>
-      <form class="max-w-xl mx-auto">
-        <div class="relative z-0 w-full mb-5 group">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="w-full px-20 mt-4 mx-auto"
+      >
+        <div className="relative z-0 w-full mb-5 group">
           <input
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            value={formik.values.shippingAddress.city}
             type="text"
-            name="city"
+            name="shippingAddress.city"
             id="city"
-            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
           />
           <label
-            for="city"
-            class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            htmlFor="city"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             City
           </label>
         </div>
-        <div class="relative z-0 w-full mb-5 group">
+        <div className="relative z-0 w-full mb-5 group">
           <input
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            value={formik.values.shippingAddress.phone}
             type="tel"
-            name="phone"
+            name="shippingAddress.phone"
             id="phone"
-            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
           />
           <label
-            for="phone"
-            class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            htmlFor="phone"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Phone Number
           </label>
         </div>
-        <div class="relative z-0 w-full mb-5 group">
+        <div className="relative z-0 w-full mb-5 group">
           <textarea
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            value={formik.values.shippingAddress.details}
             type="text"
-            name="details"
+            name="shippingAddress.details"
             id="details"
-            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
           ></textarea>
           <label
-            for="details"
-            class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            htmlFor="details"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Order Details
           </label>
         </div>
-        <button className="btn mr-2">Cash Order</button>
-        <button className="btn">Online Order</button>
+        <button type="submit" className="btn mr-2">
+          Cash Order
+        </button>
+        <button type="submit" className="btn">
+          Online Order
+        </button>
       </form>
     </>
   );
